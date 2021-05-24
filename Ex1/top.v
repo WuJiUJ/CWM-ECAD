48f0255b47c1f723dac07fd778b1869cbf59754c
Cloud User <centos@ip-172-31-41-192.eu-west-2.compute.internal>
2021-05-24 11:57:06 +0000

    A complete solutioin to Ex1

--- Ex1/top.v
+++ Ex1/top.v
@@ -33,5 +33,9 @@ module blinds(
     wire   out;
          
       //Todo: add you logic here
+	assign out = (a==1'b0&&b==1'b0) ? func[0] : 
+		(a==1'b0&&b==1'b1) ? func[1] :
+		(a==1'b1&&b==1'b0) ? func[2] :
+		func[3];
 
 endmodule
