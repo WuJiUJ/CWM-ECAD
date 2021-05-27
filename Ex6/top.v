a7b2d9a007a5fa1ddb7e7852a9c04e57018ea030
Cloud User <centos@ip-172-31-41-192.eu-west-2.compute.internal>
2021-05-27 13:53:38 +0000

    A complete solution to Ex6

--- Ex6/top.v
+++ Ex6/top.v
@@ -14,4 +14,25 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module rgb_module(
+    //Todo: add ports 
+	input [2:0] colour,
+	input clk,
+	input enable,
+	output [23:0] rgb
+    );
+
+    //IP core
+	blk_mem_gen_0 top (
+	  .clka(clk),    // input wire clka
+	  .ena(enable),      // input wire ena
+	  .wea(1'b0),      // input wire [0 : 0] wea
+	  .addra(colour),  // input wire [2 : 0] addra
+	  .dina(24'h0),    // input wire [23 : 0] dina
+	  .douta(rgb)  // output wire [23 : 0] douta
+	);
+endmodule
+
 
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -6,4 +6,140 @@
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+	reg clk;
+	reg [2:0] colour;
+	reg [2:0] previous_colour;
+	reg enable;
+	reg err;
+	reg test = 0;
+	wire [23:0] rgb;
+
+//Todo: Clock generation
+	initial
+    	begin
+	   clk = 1'b0;
+	   forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+//Colour generator
+	initial begin
+	   #(CLK_PERIOD/2)
+	   colour = 3'b000;
+	   forever begin		
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = colour + 3'b001;
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = colour + 3'b001;
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = colour + 3'b001;
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = colour + 3'b001;
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = colour + 3'b001;
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = colour + 3'b001;
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = colour + 3'b001;
+		#CLK_PERIOD
+		previous_colour = colour;
+		colour = 3'b000;
+	   end
+	end
+
+//Enable generator
+	initial begin
+	   enable = 1'b0;
+	   #(CLK_PERIOD/2)
+	   forever begin
+		#(CLK_PERIOD*8)
+		#(CLK_PERIOD*3/4)
+		enable = ~enable;
+	   end
+	end
+
+//Todo: User logic
+     initial begin
+	err=0;
+	#CLK_PERIOD
+	forever begin
+	   #CLK_PERIOD
+	   test = ~test;
+	   if(enable) begin
+		case(previous_colour)
+		   3'b000: if(rgb != 24'h000000) begin
+			   $display("***TEST FAILED! rgb should be 0 when colour=000 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		   3'b001: if(rgb != 24'h0000FF) begin
+			   $display("***TEST FAILED! rgb should be 24'h0000FF when colour=001 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		   3'b010: if(rgb != 24'h00FF00) begin
+			   $display("***TEST FAILED! rgb should be 24'h00FF00 when colour=010 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		   3'b011: if(rgb != 24'h00FFFF) begin
+			   $display("***TEST FAILED! rgb should be 24'h00FFFF when colour=011 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		   3'b100: if(rgb != 24'hFF0000) begin
+			   $display("***TEST FAILED! rgb should be 24'hFF0000 when colour=100 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		   3'b101: if(rgb != 24'hFF00FF) begin
+			   $display("***TEST FAILED! rgb should be 24'hFF00FF when colour=101 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		   3'b110: if(rgb != 24'hFFFF00) begin
+			   $display("***TEST FAILED! rgb should be 24'hFFFF00 when colour=110 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		   3'b111: if(rgb != 24'hFFFFFF) begin
+			   $display("***TEST FAILED! rgb should be 24'hFFFFFF when colour=111 rgb=%d ***", rgb);
+			   err=1;	
+			   end
+		endcase
+	   end
+	   else begin
+		if(rgb != 24'h0) begin
+		   $display("***TEST FAILED! rgb should be 0 when enable=0 rgb=%d ***", rgb);
+		   err=1;
+		end 
+	   end		
+        end
+     end
+   
+//Todo: Finish test, check for success
+	initial begin
+        #160
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+//Todo: Instantiate counter module
+	rgb_module converter (
+	.enable (enable),
+	.clk (clk),
+	.colour (colour),
+	.rgb (rgb)
+	);
+ 
+endmodule
