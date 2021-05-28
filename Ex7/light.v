f26d8ab64f912d959042677dba3eb76956c47d1b
Cloud User <centos@ip-172-31-41-192.eu-west-2.compute.internal>
2021-05-28 10:25:11 +0000

    A complete solution to Ex7

--- Ex7/light.v
+++ Ex7/light.v
@@ -14,17 +14,27 @@ module light(
 	always @(posedge clk) begin
 	   if(rst)
 	      colour = 3'b000;
-	   else
-	      case(colour)
-	         3'b000: colour = 3'b001;
-	         3'b001: colour = (button==1'b1) ? 3'b010 : 3'b001;
-	         3'b010: colour = (button==1'b1) ? 3'b011 : 3'b010;
-	         3'b011: colour = (button==1'b1) ? 3'b100 : 3'b011;
-	         3'b100: colour = (button==1'b1) ? 3'b101 : 3'b100;
-	         3'b101: colour = (button==1'b1) ? 3'b110 : 3'b101;
-	         3'b110: colour = (button==1'b1) ? 3'b001 : 3'b110;
-	         3'b111: colour = 3'b001;
-	      endcase
+	   else begin
+	      if(button == 1'b1) begin
+		case(colour)
+			3'b000: colour = 3'b001;
+			3'b001: colour = (button==1'b1) ? 3'b010 : 3'b001;
+			3'b010: colour = (button==1'b1) ? 3'b011 : 3'b010;
+			3'b011: colour = (button==1'b1) ? 3'b100 : 3'b011;
+			3'b100: colour = (button==1'b1) ? 3'b101 : 3'b100;
+			3'b101: colour = (button==1'b1) ? 3'b110 : 3'b101;
+			3'b110: colour = (button==1'b1) ? 3'b001 : 3'b110;	
+			3'b111: colour = 3'b001;
+		endcase
+	      end
+	      else begin
+		case(colour)
+			3'b000: colour = 3'b001;
+			3'b111: colour = 3'b001;
+			default: colour = colour;
+	        endcase
+	      end
+	   end
 	end             
       
 endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -20,18 +20,23 @@ module top_tb(
 	reg rst;
 	reg err;
 	reg sel;
-	reg test = 0;
 	wire [23:0] light;
 
 //Todo: Clock generation
 	initial
     	begin
-	   clk <= 1'b0;
-	   button <= 1'b0;
+	   clk = 1'b0;
 	   forever begin
-		#(CLK_PERIOD/2)
-		clk<=~clk;
-		button<=~button;
+		#(CLK_PERIOD/2) clk=~clk;
+	   end
+	end
+
+//Todo: Button generation
+	initial
+    	begin
+	   button = 1'b0;
+	   forever begin
+		#(CLK_PERIOD) button=~button;
 	   end
 	end
 
@@ -40,7 +45,7 @@ module top_tb(
 	   sel = 1'b1;
 	   #(CLK_PERIOD/2)
 	   forever begin
-		#(CLK_PERIOD*8)
+		#(CLK_PERIOD*16)
 		#(CLK_PERIOD*3/4)
 		sel = ~sel;
 	   end
@@ -49,42 +54,84 @@ module top_tb(
 //Todo: User logic
      initial begin
 	err=0;
+	rst=1;
+	#(CLK_PERIOD)
+	if(light != 24'h000000) begin
+	   $display("***TEST FAILED! light should be 0 when rst=0 and sel=1 ***", light);
+	   err=1;	
+	end
 	rst=0;
-	#CLK_PERIOD
+	#(CLK_PERIOD)
 	forever begin
-	   #CLK_PERIOD
-	   test = ~test;
-	     if(sel) begin		
-		if(light != 24'h000000) begin
-		   $display("***TEST FAILED! light should be 0 when colour=000 light=%d ***", light);
-		   err=1;	
-		   end
+	   #(CLK_PERIOD*2)
+	     if(sel) begin
 		if(light != 24'h0000FF) begin
 		   $display("***TEST FAILED! light should be 24'h0000FF when colour=001 light=%d ***", light);
 		   err=1;	
 		   end
+		#(CLK_PERIOD)
+		if(light != 24'h0000FF) begin
+		   $display("***TEST FAILED! light should be 24'h0000FF when colour=001 light=%d ***", light);
+		   err=1;	
+		   end
+		#(CLK_PERIOD)
 		if(light != 24'h00FF00) begin
 		   $display("***TEST FAILED! light should be 24'h00FF00 when colour=010 light=%d ***", light);
 		   err=1;	
 		   end
+		#(CLK_PERIOD)
+		if(light != 24'h00FF00) begin
+		   $display("***TEST FAILED! light should be 24'h00FF00 when colour=010 light=%d ***", light);
+		   err=1;	
+		   end
+		#(CLK_PERIOD)
 		if(light != 24'h00FFFF) begin
 		   $display("***TEST FAILED! rgb should be 24'h00FFFF when colour=011 light=%d ***", light);
 		   err=1;	
 		   end
+		#(CLK_PERIOD)
+		if(light != 24'h00FFFF) begin
+		   $display("***TEST FAILED! rgb should be 24'h00FFFF when colour=011 light=%d ***", light);
+		   err=1;	
+		   end
+		#(CLK_PERIOD)
 		if(light != 24'hFF0000) begin
 		   $display("***TEST FAILED! rgb should be 24'hFF0000 when colour=100 light=%d ***", light);
 		   err=1;	
 		   end
+		#(CLK_PERIOD)
+		if(light != 24'hFF0000) begin
+		   $display("***TEST FAILED! rgb should be 24'hFF0000 when colour=100 light=%d ***", light);
+		   err=1;	
+		   end
+		#(CLK_PERIOD)
 		if(light != 24'hFF00FF) begin
 		   $display("***TEST FAILED! rgb should be 24'hFF00FF when colour=101 light=%d ***", light);
 		   err=1;	
 		   end
+		#(CLK_PERIOD)
+		if(light != 24'hFF00FF) begin
+		   $display("***TEST FAILED! rgb should be 24'hFF00FF when colour=101 light=%d ***", light);
+		   err=1;	
+		   end
+		#(CLK_PERIOD)
 		if(light != 24'hFFFF00) begin
 		   $display("***TEST FAILED! rgb should be 24'hFFFF00 when colour=110 light=%d ***", light);
 		   err=1;	
 		   end
-		if(light != 24'hFFFFFF) begin
-		   $display("***TEST FAILED! rgb should be 24'hFFFFFF when colour=111 light=%d ***", light);
+		#(CLK_PERIOD)
+		if(light != 24'hFFFF00) begin
+		   $display("***TEST FAILED! rgb should be 24'hFFFF00 when colour=110 light=%d ***", light);
+		   err=1;	
+		   end
+		#(CLK_PERIOD)
+		if(light != 24'h0000FF) begin
+		   $display("***TEST FAILED! light should be 24'h0000FF when colour=001 light=%d ***", light);
+		   err=1;	
+		   end
+		#(CLK_PERIOD)
+		if(light != 24'h0000FF) begin
+		   $display("***TEST FAILED! light should be 24'h0000FF when colour=001 light=%d ***", light);
 		   err=1;	
 		   end
 	     end
@@ -99,7 +146,7 @@ module top_tb(
    
 //Todo: Finish test, check for success
 	initial begin
-        #160
+        #330
         if (err==0)
           $display("***TEST PASSED! :) ***");
         $finish;
